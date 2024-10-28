87d64b8d198ed9d430ba554ce940538a483116ce
Jake <96874701+Jakeplays12@users.noreply.github.com>
2024-10-28 00:52:32 +0000

    Fix for "articles" collection.
    Fix for "articles" collection picking up everything but articles.

--- .eleventy.js
+++ .eleventy.js
@@ -13,22 +13,19 @@ module.exports = function (eleventyConfig) {
   // Add the markdown-it-anchor plugin
   md.use(markdownItAnchor);
 
-  // Add plugins
   eleventyConfig.addPlugin(pluginTOC, {
     tags: ['h1','h2', 'h3', 'h4'],
     wrapper: '',
     
   });
 
-  eleventyConfig.setLibrary("md", md); // Set the markdown library to Eleventy
+  eleventyConfig.setLibrary("md", md);
 
-  // Copy assets
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("assets/");
   eleventyConfig.addPassthroughCopy("**/*.jpg");
   eleventyConfig.addPassthroughCopy("robots.txt");
 
-  // Custom filters
   eleventyConfig.addFilter("sortByDate", function (posts) {
     return posts.slice().sort((a, b) => new Date(b.data?.date || b.date) - new Date(a.data?.date || a.date));
   });
@@ -38,8 +35,9 @@ module.exports = function (eleventyConfig) {
     return new Date(dateString).toLocaleDateString(undefined, options);
   });
 
-  // Custom collection
-  eleventyConfig.addCollection("articles", function (collectionApi) {
-    return collectionApi.getAll().filter(item => !item.url.startsWith("/articles/"));
-  });
+  // 28/10 Updated to work correctly. this will only pick up /article/** now.
+eleventyConfig.addCollection("articles", function (collectionApi) {
+  return collectionApi.getAll().filter(item => item.url && item.url.startsWith("/article/"));
+});
+
 };
